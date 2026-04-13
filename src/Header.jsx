4ed56dfcc81110d942a5fa11427105c2ee69e739
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2026-04-13 12:31:46 +0530

    Header jsx file of the application

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+
+import "./header.css";
+function Header(){
+
+  return(
+    <div className="container">
+      <div className="header">
+        <h1 className="page-title">Budget Tracker</h1>
+        </div>
+      </div>
+  )
+}
+
+export default Header;
